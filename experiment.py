a1c31be17e7fb6eab795a85133797e3419e030dc
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2015-01-29 09:24:13 -0800

    2nd commit
    everything we’ve done so far

--- experiment.py
+++ experiment.py
@@ -1,38 +1,36 @@
-from wallace.networks import Chain
-from wallace.processes import RandomWalkFromSource
+from wallace.processes import Process
 from wallace.recruiters import PsiTurkRecruiter
-from wallace.agents import ReplicatorAgent
+from wallace.agents import ReplicatorAgent, Agent
 from wallace.experiments import Experiment
 from wallace.sources import Source
+import json
+from wallace.information import Gene, Meme
+from wallace.networks import Network
+import math
 
 
-class Bartlett1932(Experiment):
+class Rogers(Experiment):
     def __init__(self, session):
-        super(Bartlett1932, self).__init__(session)
+        super(Rogers, self).__init__(session)
 
-        self.task = "Transmission chain"
-        self.num_agents = 10
-        self.num_steps = self.num_agents - 1
-        self.agent_type = ReplicatorAgent
-        self.network = Chain(self.agent_type, self.session)
-        self.process = RandomWalkFromSource(self.network)
+        self.task = "Rogers network game"
+        self.num_agents_per_generation = 3
+        self.num_generations = 3
+        self.num_agents = self.num_agents_per_generation*self.num_generations
+        self.agent_type = RogersAgent
+        self.network = RogersNetwork(self.agent_type, self.session, self.num_agents_per_generation, self.num_generations)
+        self.process = RogersNetworkProcess(self.network)
         self.recruiter = PsiTurkRecruiter
 
         # Setup for first time experiment is accessed
         if not self.network.sources:
-            source = WarOfTheGhostsSource()
+            source = RogersSource()
             self.network.add_source_global(source)
             print "Added initial source: " + str(source)
 
     def newcomer_arrival_trigger(self, newcomer):
 
         self.network.add_agent(newcomer)
-
-        # If this is the first participant, link them to the source.
-        if len(self.network.agents) == 1:
-            source = self.network.sources[0]
-            source.connect_to(newcomer)
-            self.network.db.commit()
 
         # Run the next step of the process.
         self.process.step()
@@ -53,19 +51,149 @@ class Bartlett1932(Experiment):
             self.recruiter().close_recruitment()
         else:
             # Otherwise recruit a new participant.
-            self.recruiter().recruit_new_participants(self, n=1)
+            self.recruiter().recruit_new_participants(self, n=num_agents_per_generation)
 
     def is_experiment_over(self):
         return len(self.network.agents) == self.num_agents
 
 
-class WarOfTheGhostsSource(Source):
-    """A source that transmits the War of Ghosts story from Bartlett (1932).
+class RogersSource(Source):
+    """A source that initializes agents as asocial learners
     """
 
-    __mapper_args__ = {"polymorphic_identity": "war_of_the_ghosts_source"}
+    __mapper_args__ = {"polymorphic_identity": "rogers_source"}
 
     @staticmethod
     def _data():
-        with open("static/stimuli/ghosts.md", "r") as f:
-            return f.read()
+        return {"learner": "asocial"}
+        #return json.dumps(data)
+
+
+    def transmit(self, other_node):
+        info = Gene(
+            origin=self,
+            origin_uuid=self.uuid,
+            contents=self._data())
+
+        super(Source,self).transmit(info,other_node)
+
+class RogersNetwork(Network):
+    """In a Rogers Network agents are arranged into genenerations of a set size and each agent
+    is connected to all agents in the previous generation (the first generation agents are
+    connected to a source)
+    """ 
+
+    def __init__(self, agent_type, db, agents_per_generation=10, num_generations=10):
+        self.db = db
+        self.agents_per_generation = agents_per_generation
+        self.num_generations = num_generations
+        super(RogersNetwork, self).__init__(agent_type, db)
+
+    @property
+    def first_agent(self):
+        if len(self.agents) > 0:
+            return self.db.query(Agent)\
+                .order_by(Agent.creation_time)\
+                .filter(Agent.status != "failed")\
+                .first()
+        else:
+            return None
+
+    @property
+    def last_agent(self):
+        if len(self.agents) > 0:
+            return self.db.query(Agent)\
+                .order_by(Agent.creation_time.desc())\
+                .filter(Agent.status != "failed")\
+                .first()
+        else:
+            return None
+
+    def add_agent(self, newcomer):
+        if len(self.sources) is 0:
+            self.db.add(RogersSource())
+            self.db.commit()
+
+        self.db.add(newcomer)
+        self.db.commit()
+
+        if len(self.agents) <= self.agents_per_generation:
+            self.sources[0].connect_to(newcomer)
+            self.db.commit()
+        else:
+            newcomer_generation = math.floor(((len(self.agents)-1)*1.0)/self.agents_per_generation)
+            min_previous_generation = (newcomer_generation-1)*self.agents_per_generation
+            previous_generation_agents = self.db.query(Agent)\
+            .filter(Agent.status != "failed")\
+            .order_by(Agent.creation_time)[min_previous_generation:(min_previous_generation+self.agents_per_generation)]    
+            
+            for a in previous_generation_agents:
+                a.connect_to(newcomer)
+            self.db.commit()
+
+        return newcomer
+
+    def agents_of_generation(generation):
+        first_index = generation*self.agents_per_generation
+        last_index = first_index+(agents_per_generation-1)
+        return self.agents[first_index:last_index]
+
+
+class RogersNetworkProcess(Process):
+
+    def __init__(self, network):
+        super(RogersNetworkProcess, self).__init__(network)
+
+    def step(self, verbose=True):
+
+        newcomer = self.network.last_agent
+        current_generation = floor((len(self.network.agents)*1.0-1)/self.network.agents_per_generation)
+
+        if (current_generation==0):
+            self.network.sources[0].transmit(self.network.agents[len(self.network.agents)])
+        else:
+            parent = None
+            potential_parents = agents_of_generation(current_generation-1)
+            potential_parent_fitnesses = [p.fitness() for p in potential_parents]
+            potential_parent_probabilities = [(f/(1.0*sum(potential_parent_fitnesses))) for f in potential_parent_fitnesses]
+            rnd = random.random()
+            temp = 0.0
+            for i, probability in enumerate(potential_parent_probabilities):
+                temp += probability
+                if rnd < temp:
+                    parent = potential_parents[i]
+            parent.transmit(newcomer, info_type=Gene)
+            newcomer.receive_all()
+
+            gene = newcomer.latest_information_received(info_type=Gene)
+
+            if (gene.contents["learner"] == "social"):
+                rnd = random.randint(0,(self.network.agents_per_generation-1))
+                cultural_parent = potential_parents[rnd]
+                cultural_parent.transmit(newcomer,info_type=Meme)
+                newcomer.receive_all()
+            else if (gene.contents["learner"] == "asocial"):
+                pass
+            else:
+                raise AssertionError("Learner gene set to non-coherent value")
+
+
+class RogersAgent(Agent):
+
+     __mapper_args__ = {"polymorphic_identity": "rogers_agent"}
+
+    def transmit(self, other_node, info_type=Info):
+        info = info_type\
+            .query\
+            .filter_by(origin_uuid=self.uuid)\
+            .order_by(desc(info_type.creation_time))\
+            .first()
+
+        super(Source,self).transmit(info,other_node)
+
+    def update(self, info):
+        info.copy_to(self)
+
+
+
+
---
+++ test_rogers_network.py
@@ -0,0 +1,48 @@
+from wallace import networks, agents, db, sources
+from experiment import RogersNetwork
+
+
+class TestNetworks(object):
+
+    def setup(self):
+        self.db = db.init_db(drop_all=True)
+
+    def teardown(self):
+        self.db.rollback()
+        self.db.close()
+
+    def test_create_rogers_network_small(self):
+        net = RogersNetwork(agents.ReplicatorAgent, self.db, agents_per_generation=3)
+
+        newcomers = []
+        for i in range(12):
+            agent = agents.ReplicatorAgent()
+            newcomers.append(agent)
+            net.add_agent(agent)
+
+        assert not newcomers[0].has_connection_to(newcomers[6])
+        assert not newcomers[0].has_connection_to(newcomers[7])
+        assert not newcomers[0].has_connection_to(newcomers[8])
+
+        assert newcomers[0].has_connection_to(newcomers[3])
+        assert newcomers[0].has_connection_to(newcomers[4])
+        assert newcomers[0].has_connection_to(newcomers[5])
+
+        assert newcomers[11].has_connection_from(newcomers[6])
+        assert newcomers[11].has_connection_from(newcomers[7])
+        assert newcomers[11].has_connection_from(newcomers[8])
+
+        assert len(net.vectors) == 30
+
+
+    def test_create_rogers_network_large(self):
+        net = RogersNetwork(agents.ReplicatorAgent, self.db, agents_per_generation=100)
+
+        newcomers = []
+        for i in range(400):
+            agent = agents.ReplicatorAgent()
+            newcomers.append(agent)
+            net.add_agent(agent)
+            print i
+
+        print len(net.vectors)
