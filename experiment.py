35b29386068003915dcbedd60795a58b0351a26f
Thomas Morgan <thomas.j.h.morgan@gmail.com>
2015-08-06 12:10:07 -0700

    add data check post submission

--- experiment.py
+++ experiment.py
@@ -204,6 +204,34 @@ class RogersExperiment(Experiment):
         is_passing = avg >= self.min_acceptable_performance
         return is_passing
 
+    def check_participant_data(self, participant=None):
+
+        if participant is None:
+            raise ValueError("check_participant_data must be passed a participant, not None")
+
+        participant_uuid = participant.uniqueid
+        key = participant_uuid[0:5]
+
+        self.log("{} Checking participant data".format(key))
+
+        nodes = Node.query.filter_by(participant_uuid=participant_uuid).all()
+
+        if len(nodes) != self.experiment_repeats + self.practice_repeats:
+            self.log("{} Participant has {} nodes - this is not the correct number. Data check failed".format(key))
+            return False
+
+        nets = [n.network_uuid for n in nodes]
+        if len(nets) != len(set(nets)):
+            self.log("{} Participant participated in the same network multiple times. Data check failed".format(key))
+            return False
+
+        if None in [n.fitness for n in nodes]:
+            self.log("{} Some of participants nodes are missing a fitness. Data check failed".format(key))
+            return False
+
+        self.log("{} Data check passed.".format(key))
+        return True
+
 
 class LearningGene(Gene):
     __mapper_args__ = {"polymorphic_identity": "learning_gene"}
